d793852e1dc234cf5f6d076c8e1348de22e0edc5
Dan-Sillva <danilo.official22@hotmail.com>
2021-12-09 22:57:27 -0300

    selection-sort-v2

--- selection sort/test.cpp
+++ selection sort/test.cpp
@@ -1,19 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void selection_sort(int n, int vet[]);
+/*
+ * Danilo Araujo Silva
+*/
+
+using namespace std;
+
+void selection_sort(int tamanho_vetor, int point, int *vet);
+void fill_vector(int final, int tamanho_vetor, int *vet);
+
+void print_vector(int tamanho_vetor, int vet[])
+{
+    for (int i = 0; i < tamanho_vetor; i++)
+        printf("[%d]", vet[i]);
+};
 
 int main()
 {
-    int vet[3];
-    selection_sort(5, vet);
+    clock_t time = clock();
+
+    int tamanho_vetor = 10;
+    int vet[tamanho_vetor];
+
+    fill_vector(10, tamanho_vetor, vet);
+    selection_sort(tamanho_vetor, 0, vet);
+
+    print_vector(tamanho_vetor, vet);
+
+    time = clock() - time;
+
+    printf("\n\n-Ordenacao de vetor de %d posicoes;\n", tamanho_vetor);
+    printf("-Tempo de execucao : %.7f segundos", ((double)time) / ((CLOCKS_PER_SEC)));
 }
 
-void selection_sort(int n, int vet[])
+void fill_vector(int final, int tamanho_vetor, int *vet)
 {
-
-    for (int i = 1; i < n; i++)
+    srand(time(NULL));
+    for (int i = 0; i < tamanho_vetor; i++)
     {
-        n--;
+        vet[i] = rand() % final + 1;
     }
-    printf("%d", n);
+};
+
+void selection_sort(int tamanho_vetor, int point, int *vet)
+{
+    if (point != tamanho_vetor)
+    {
+        for (int i = point; i < tamanho_vetor; i++)
+        {
+            if (vet[point] >= vet[i])
+            {
+                int aux;
+                aux = vet[point];
+                vet[point] = vet[i];
+                vet[i] = aux;
+            }
+        }
+        selection_sort(tamanho_vetor, point + 1, vet);
+    }
 };
