666b9bac6fbc317fc8f52126263ed3ba231ef571
José Antonio de Andrade <andradejoseantonio0@gmail.com>
2021-12-14 16:04:42 -0300

    merge sort - prenechimento de valores aleatórios no vetor e medição do tempo ded execução

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,3 +1,18 @@
+/*
+* José Antonio de Andrade Siqueira - 2010072
+*
+ * Ordenacao de vet[10] = 0.0000001 segundos
+ *
+ * Ordenacao de vet[100] = 0.0032000 segundos
+ * 
+ * Ordenacao de vet[1000] = 0.03440000 segundos
+ * 
+ * Ordenacao de vet[10000] = 0.4200000 segundos
+ * 
+ * Ordenacao de vet[100000] = 3.2861000 segundos
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h>
@@ -11,25 +26,29 @@
 void Merge(int *A, int *E, int esqCount, int *D, int dirCount);
 
 // Função recursiva para ordenar um array de inteiros
-void MergeSort(int *A, int numeroElementos);
+void MergeSort(int *A, int numero_elementos);
 
+void preencher_vetor(int tamanho_vetor, int *A);
 
 /* ONDE A MÁGICA ACONTECE */
 
+int tamanho_vetor = 100000; 
+
+
 int main() {
 
-	int tamanho_vetor = 100; 
 	int A[tamanho_vetor]; 
-	int i, numeroElementos;
+	int i, numero_elementos;
 
 	clock_t tempo_execucao = clock();
 
-	numeroElementos = sizeof(A)/sizeof(A[0]); 
+	numero_elementos = sizeof(A)/sizeof(A[0]); 
 
-	MergeSort(A, numeroElementos);
+	preencher_vetor(tamanho_vetor, A);
+	MergeSort(A, numero_elementos);
 
    // mostrando os elementos após ordenação
-	for(i = 0; i < numeroElementos; i++){
+	for(i = 0; i < numero_elementos; i++){
 		printf("%d ", A[i]);
 	}
 
@@ -65,27 +84,38 @@ void Merge(int *A, int *E, int esqCount, int *D, int dirCount) {
 }
 
 
-void MergeSort(int *A, int numeroElementos) {
+void MergeSort(int *A, int numero_elementos) {
 	int meio, *E, *D, i;
-	if(numeroElementos < 2) return; // Se o array tiver menos de dois elementos, não acontece nada
+	if(numero_elementos < 2) return; // Se o array tiver menos de dois elementos, não acontece nada
 
-	meio = numeroElementos/2;
+	meio = numero_elementos/2;
 	
 
    // cria arrays da esquerda e direita
    // elementos do index 0 até meio-1 farão parte do array da esquerda
    // e elementos do meio até n-1 farão parte do array da direita
 	E = (int*)malloc(meio*sizeof(int)); 
-	D = (int*)malloc((numeroElementos - meio)*sizeof(int)); 
+	D = (int*)malloc((numero_elementos - meio)*sizeof(int)); 
 	
 
 	for(i = 0; i < meio; i++) E[i] = A[i]; // cria o subarray da esquerda
-	for(i = meio; i < numeroElementos; i++) D[i-meio] = A[i]; // cria o subarray da esquerda
+	for(i = meio; i < numero_elementos; i++) D[i-meio] = A[i]; // cria o subarray da esquerda
 
 
 	MergeSort(E, meio);  // ordenando subarray da esquerda
-	MergeSort(D, numeroElementos -meio);  // ordenando subarray da direita
-	Merge(A, E, meio, D, numeroElementos - meio);  // Fazendo o Merge de E e D em A, como uma lista ordenada
+	MergeSort(D, numero_elementos -meio);  // ordenando subarray da direita
+	Merge(A, E, meio, D, numero_elementos - meio);  // Fazendo o Merge de E e D em A, como uma lista ordenada
         free(E);
         free(D);
 }
+
+void preencher_vetor(int tamanho_vetor, int *A)
+{
+	int valor_final = tamanho_vetor + 1;
+
+    srand(time(NULL));
+    for (int i = 0; i < tamanho_vetor; i++)
+    {
+        A[i] = rand() % valor_final;
+    }
+};
